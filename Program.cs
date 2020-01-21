df03678da5d8225f3d737d4123828c2079d33c0a
VR56 <53446898+VR56@users.noreply.github.com>
2020-01-21 13:07:12 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Project_1
+{
+    public sealed class SingletonExample
+    {
+        private SingletonExample()
+        { }
+        private static SingletonExample instance=null;
+        public static SingletonExample Instance
+            {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SingletonExample();
+                }
+
+                return instance;
+            }
+            }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            SingletonExample s1 = SingletonExample.Instance;
+            SingletonExample s2 = SingletonExample.Instance;
+            if(s1==s2)
+            Console.WriteLine("Even though two instances are tried to be created here but both the instances are same and hence only one instance is being created here logically");
+  
+        }
+    }
+}
